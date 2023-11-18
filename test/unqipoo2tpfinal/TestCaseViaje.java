d1aa88f17d372b45ff501e232cb3dcaf39c7b1b2
Franco Mamani <thefk2000@gmail.com>
2023-11-18 16:36:34 -0300

    testViaje

--- test/unqipoo2tpfinal/TestCaseViaje.java
+++ test/unqipoo2tpfinal/TestCaseViaje.java
@@ -2,6 +2,9 @@ package unqipoo2tpfinal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +15,12 @@ class TestCaseViaje {
 	private TerminalPortuaria terminalDeLlegada;
 	private List<Tramo> tramos;
 	private CircuitoMaritimo circuito;
+	private Buque buque;
+	private LocalDate fechaInicio = LocalDate.of(2023, 11, 18);
 	
 	@BeforeEach
 	public void setUp() {
-		
+		viaje = new Viaje(buque, fechaInicio, terminalDeSalida, terminalDeLlegada, circuito);
 	}
 	
 	@Test 
